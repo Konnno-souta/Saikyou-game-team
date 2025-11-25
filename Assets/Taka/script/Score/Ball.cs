e4044de2c8fb66c987a3f6e4a11d992d3ea08ff1
taka <sdt2495gp0011@edu.sca.ac.jp>
2025-11-25 11:03:01 +0900

    test

--- Assets/Taka/script/Score/Ball.cs
+++ Assets/Taka/script/Score/Ball.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public enum BallType { Red, Green, Gold, SpeedUp }
+    public enum BallType { Red, Green, Gold, SpeedUp,Bom,Time }
     public BallType ballType;
 }
