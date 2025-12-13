61150564d676d5563a790932ebac8be0761f79d7
takahashi <sdt2495gp0011@edu.sca.ac.jp>
2025-12-13 16:27:30 +0900

    リザルト仮

--- Assets/Taka/script/SceneChanger.cs
+++ Assets/Taka/script/SceneChanger.cs
@@ -1,54 +1,35 @@
 using UnityEngine;
-using UnityEngine.UI;
+
 using UnityEngine.SceneManagement;
 
-public class SceneChanger_NoCoroutine : MonoBehaviour
+public class SceneChanger : MonoBehaviour
+
 {
-    public Image fadeImage;
-    public float fadeDuration = 1f;
 
-    private bool isFadingOut = false;
-    private bool isFadingIn = true;
-    private float timer = 0f;
-    private string nextScene;
+    public void GoTitle()
 
-    void Start()
     {
-        // 開始時はフェードインするため、Image は黒で始めておくこと
-        fadeImage.color = new Color(0, 0, 0, 1f);
+
+        SceneManager.LoadScene("Title");
+
     }
 
-    void Update()
+    public void GoPlayer()
+
     {
-        if (isFadingIn)
-        {
-            timer += Time.deltaTime;
-            float alpha = 1f - (timer / fadeDuration);
-            fadeImage.color = new Color(0, 0, 0, alpha);
 
-            if (timer >= fadeDuration)
-            {
-                isFadingIn = false;
-                timer = 0f;
-            }
-        }
-        else if (isFadingOut)
-        {
-            timer += Time.deltaTime;
-            float alpha = (timer / fadeDuration);
-            fadeImage.color = new Color(0, 0, 0, alpha);
+        SceneManager.LoadScene("Player");
 
-            if (timer >= fadeDuration)
-            {
-                SceneManager.LoadScene(nextScene);
-            }
-        }
     }
 
-    public void SceneChanger(string sceneName)
+    public void GoResult()
+
     {
-        nextScene = sceneName;
-        isFadingOut = true;
-        timer = 0f;
+
+        SceneManager.LoadScene("Result");
+
     }
+
 }
+
+
