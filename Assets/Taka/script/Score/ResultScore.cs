a915c593e72ae8eec2cd70993652eaf3a1143985
taka <sdt2495gp0011@edu.sca.ac.jp>
2025-12-17 19:28:14 +0900

    score

---
+++ Assets/Taka/script/Score/ResultScore.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScore : MonoBehaviour
+{
+    public Text resultScoreText;
+    public Text highScoreText;
+
+    void Start()
+    {
+        resultScoreText.text = "Score: " + ScoreManager.score.ToString();
+        highScoreText.text = "HighScore: " + ScoreManager.highScore.ToString();
+    }
+}
