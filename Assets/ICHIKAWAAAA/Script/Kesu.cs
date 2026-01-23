6a98a33703400a94c082ad11df63aabde7c36f91
aaa <aaa>
2026-01-23 13:46:23 +0900

    玉編集　もう少しで完成するね！　たのしみ～

---
+++ Assets/ICHIKAWAAAA/Script/Kesu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Kesu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 10.0f);
+    }
+
+    
+}
